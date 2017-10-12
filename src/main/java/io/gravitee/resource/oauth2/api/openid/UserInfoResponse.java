d922552e00ab81dec845c3b334b88a84827faac1
brasseld <brasseld@gmail.com>
2017-10-12 22:07:12 +0200

    feat(openid): Add userinfo service for OpenID Connect
    Closes gravitee-io/issues#803

---
+++ src/main/java/io/gravitee/resource/oauth2/api/openid/UserInfoResponse.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.resource.oauth2.api.openid;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class UserInfoResponse {
+
+    private final boolean success;
+
+    private final String payload;
+
+    private final Throwable throwable;
+
+    public UserInfoResponse(final boolean success, final String payload) {
+        this.success = success;
+        this.payload = payload;
+        this.throwable = null;
+    }
+
+    public UserInfoResponse(final Throwable throwable) {
+        this.success = false;
+        this.throwable = throwable;
+        this.payload = throwable.getMessage();
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+}
